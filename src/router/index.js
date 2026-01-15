8b39f42226c1f8863d10b88b611ce2b4b5b2422c
kevin.zhu <kevin.zhu@nextocal.com>
2026-01-15 19:53:47 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '../components/Board.vue'
+import About from '../components/About.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Board',
     component: Board
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
   }
 ]
 
